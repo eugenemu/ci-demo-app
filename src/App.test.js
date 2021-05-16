c6d5b956d5c4b40d2b75664ebdc81cff728ef4a6
James <james@cloudspeakers.dev>
2021-05-16 01:32:06 -0400

    Add config

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
+import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
+
+it('renders welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('Learn React')).toBeInTheDocument();
 });
